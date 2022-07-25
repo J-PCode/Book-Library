828ce10f3b3c32b681fabe40ba10be1b54abf7cd
J-PCode <89179060+J-PCode@users.noreply.github.com>
2022-07-25 14:43:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,32 @@ class Book {
 }
 
 // Class: Show UI tasks
+class UI {
+  static displayBooks() {
+    const books = Store.getBooks();
+    
+    books.forEach((book) => UI.addBookToList(book));
+  }
+  
+    static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+      <td>${book.read}</td>     
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      `;
+      list.appendChild(row);
+    }
+  static deleteBook(el) {
+    if(el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
 
 // Store Class: Handles Storage
 
@@ -20,5 +46,26 @@ e.preventDefault();
 const title = document.querySelector('#title').value;
 const author = document.querySelector('#author').value;
 const read = document.querySelector('#read').value;
-const pages = document.querySelector('#pages).value;
+const pages = document.querySelector('#pages').value;
+  
+   // Validate
+  if(title === '' || author === '' || pages === '') {
+    UI.showAlert('Please fill in all fields', 'danger');
+  } else {
+    // Instatiate book
+    const book = new Book(title, author, read, pages);
+
+    // Add Book to UI
+    UI.addBookToList(book);
+
+    // Add book to store
+    Store.addBook(book);
+
+    // Show success message
+    UI.showAlert('Book Added', 'success');
+
+    // Clear fields
+    UI.clearFields();
+  }
+});
 

7e414f297de3109b36b90cdb267f71710a0c0a43
J-PCode <orjala.juho@gmail.com>
2022-07-29 18:37:08 +0300

    Now it's working. Added local storage, CSS changed colors. Finished this project for now

--- script.js
+++ script.js
@@ -11,23 +11,7 @@ class Book {
 // UI Class: Show UI tasks: displaybooks, remove book, add book, 
 class UI {
   static displayBooks() {
-    const StoredBooks = [
-      {
-        title:'Book One',
-        author: 'John Doe',
-        pages: '210',
-        read: 'read'
-      },
-      {
-        title:'Book two',
-        author: 'Jane Doe',
-        pages: '310',
-        read: 'Not Read'
-      }
-    ];
-
-    const books = StoredBooks;
-    
+     const books = Store.getBooks();
     books.forEach((book) => UI.addBookToList(book));
   }
 
@@ -59,6 +43,7 @@ class UI {
 
       static deleteBook(el){
         if(el.classList.contains('delete')){
+          console.log(el.parentElement.parentElement)
           el.parentElement.parentElement.remove();
           UI.showAlert('Book Removed', 'success')
         }
@@ -94,6 +79,37 @@ class UI {
   }
     // Store Class: Handles Storage
 
+    class Store {
+      static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null){
+          books = [];
+        } else{
+          books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+      }
+      static addBook(book){
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+      }
+
+      static removeBook(title){
+        const books = Store.getBooks();
+        
+        books.forEach((book, index) => {
+          console.log(book.title, title)
+          if(book.title === title) {
+            books.splice(index, 1);
+          }          
+        });
+        localStorage.setItem('books', JSON.stringify(books));
+      }
+    }
     // Event: Display Books
     document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
@@ -120,6 +136,9 @@ class UI {
       // Add book to UI
       UI.addBookToList(book);
 
+      // Add book to store
+      Store.addBook(book)
+
       // Show success message
       UI.showAlert('Book Added', 'success');
 
@@ -130,6 +149,14 @@ class UI {
 
     // Event: Remove a Book
     document.querySelector('#book-list').addEventListener('click', (e) => {
+      
+      // Remove book from UI
       UI.deleteBook(e.target);
+
+      // Remove book from store
+      
+      Store.removeBook(e.target.parentElement.parentElement.firstElementChild.textContent);
+      // toggle read or not read
       UI.toggleRead(e.target);
+
     })
